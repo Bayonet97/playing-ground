12027e5c114a4d1173b82a8181a7f8a8efb678aa
Raphael23 <43172440+Raphael23@users.noreply.github.com>
2021-02-24 17:21:55 +0100

    Initial commit

---
+++ playing-ground/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace playing_ground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 2020;
+
+            int newYear = year++;
+            int anotherYear = ++year;
+
+            Console.WriteLine($"Year++: {newYear}, ++Year: {anotherYear}");
+        }
+    }
+}
